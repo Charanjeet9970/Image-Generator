4c510d1d340d16c189ff3980412dc165c92a28b7
Charanjeet <sachdevcharan903@gmail.com>
2025-09-27 20:20:22 +0530

    yo

--- server/index.js
+++ server/index.js
@@ -8,10 +8,7 @@ import generateImageRoutes from './routes/generateImage.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: ["https://image-generator-w8ya.onrender.com", "http://localhost:3000"],
-  methods: ["GET","POST"],
-}));
+app.use(cors());
 app.use(express.json({limit: '100mb' }));
 app.use(express.urlencoded({limit: '100mb', extended: true }));
 
