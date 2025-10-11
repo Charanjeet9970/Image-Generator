19eda65f18af12cf0dc185ff222d6d0f5913229b
Charanjeet <sachdevcharan903@gmail.com>
2025-10-11 15:34:43 +0530

    he

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ import generateImageRoutes from './routes/generateImage.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://aiimagegenerator21.netlify.app", "http://localhost:3000"], // Allow your frontend URLs
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json({limit: '100mb' }));
 app.use(express.urlencoded({limit: '100mb', extended: true }));
 
